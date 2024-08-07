8e64a2cc178590f05a18980c3ca78115171aef02
Mateo Vuković <176288374+vukovicdmateo@users.noreply.github.com>
2024-08-07 13:10:07 +0200

    Implement tailwind styling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
   future: {
     compatibilityVersion: 4
-  }
+  },
+
+  modules: ["@nuxtjs/tailwindcss"]
 })
