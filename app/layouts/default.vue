0f10a9c8c9c6197a4572063127f509257a50c1fd
Mateo Vuković <176288374+vukovicdmateo@users.noreply.github.com>
2024-08-07 13:14:49 +0200

    Introduce components

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,16 +4,7 @@
             <div>
                 <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">Mateo Vuković</NuxtLink>
             </div>
-            <nav class="font-mono">
-                <ul class="flex space-x-4">
-                    <li>
-                    <NuxtLink to="/">Main</NuxtLink>
-                    </li>
-                    <li>
-                    <NuxtLink to="/about">About</NuxtLink>
-                    </li>
-                </ul>
-            </nav>
+            <Menu />
         </header>
 
         <main class="p-2">
