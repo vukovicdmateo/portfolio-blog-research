27a41c454d320a72c34dc07a85246ef33913d323
Mateo Vuković <176288374+vukovicdmateo@users.noreply.github.com>
2024-08-13 02:17:48 +0200

    Introduce slot data in ContentDoc

--- app/pages/blog/[...slug].vue
+++ app/pages/blog/[...slug].vue
@@ -1,5 +1,7 @@
 <template>
-    <article class="prose dark:prose-invert prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300">
-        <ContentDoc />
+    <article class="prose dark:prose-invert prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300 max-w-none">
+        <ContentDoc v-slot="{ doc }">
+            <ContentRenderer :value="doc" />
+        </ContentDoc>
     </article>
 </template>
