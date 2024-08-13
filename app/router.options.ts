5bddf18eaf99dd4d14dbf4d9b75851c2f37bbd22
Mateo Vuković <176288374+vukovicdmateo@users.noreply.github.com>
2024-08-13 18:34:57 +0200

    Implement smooth scrolling in table of contents

---
+++ app/router.options.ts
@@ -0,0 +1,23 @@
+import type { RouterConfig } from "nuxt/schema";
+
+export default <RouterConfig>{
+    scrollBehavior(to, from, savedPosition){
+
+        if(savedPosition) {
+            return savedPosition
+        }
+
+        if(to.hash) {
+            return {
+                el: to.hash,
+                behavior: 'smooth',
+                top: 30
+            }
+        }
+
+
+        return {
+            left: 0, top: 0, behavior: 'smooth'
+        }
+    }
+}
