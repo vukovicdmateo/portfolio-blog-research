27c8505b1f91130f6f4a3cb7bdf8b14b307c3ce7
Mateo Vuković <176288374+vukovicdmateo@users.noreply.github.com>
2024-08-14 18:33:23 +0200

    Handle 404 errors

---
+++ app/error.vue
@@ -0,0 +1,15 @@
+<template>
+    <NuxtLayout>
+        <section>
+            <h1 class="text-4xl mb-4">Opps {{  error.statusCode }}</h1>
+
+            <h2 class="text-xl">
+                {{  error.statusMessage }}
+            </h2>
+        </section>
+    </NuxtLayout>
+</template>
+
+<script setup>
+defineProps({ error: Object})
+</script>
--- app/pages/blog/[...slug].vue
+++ app/pages/blog/[...slug].vue
@@ -1,6 +1,10 @@
 <template>
     <article class="prose dark:prose-invert prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300 max-w-none">
-        <ContentDoc v-slot="{ doc }">
+        <ContentDoc>
+            <template #not-found>
+                <h1>Document not found</h1>
+            </template>
+          <template v-slot="{ doc }">
             <div class="grid grid-cols-6 gap-16">
                 <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
                     <ContentRenderer :value="doc" />
@@ -16,6 +20,7 @@
                     </aside>
                 </div>
             </div>
+          </template>
            
         </ContentDoc>
     </article>
