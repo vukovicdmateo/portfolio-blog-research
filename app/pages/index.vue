7a2bd163dd9d6e3fae3a343cbc9b241963a03e31
Mateo Vuković <176288374+vukovicdmateo@users.noreply.github.com>
2024-08-19 19:13:29 +0200

    Design main page: reuse components & props in MDC

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,35 +1,5 @@
 <template>
-    <div>
-        Hello from the main page!
-    </div>
+  <article class="prose dark:prose-invert max-w-none">
+    <ContentDoc path="/" />
+  </article>
 </template>
-
-<script setup lang="ts">
-
-useSeoMeta({
-  title: 'Main page',
-  description: 'This is the main page',
-  ogTitle: 'Main page',
-  ogDescription: 'This is the main page',
-  ogImage: '[og:image]',
-  ogUrl: '[og:url]',
-  twitterTitle: 'Main page',
-  twitterDescription: '[twitter:description]',
-  twitterImage: '[twitter:image]',
-  twitterCard: 'summary'
-})
-
-useHead({
-  htmlAttrs: {
-    lang: 'en'
-  },
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/png',
-      href: '/favicon.ico'
-    }
-  ]
-})
-
-</script>
